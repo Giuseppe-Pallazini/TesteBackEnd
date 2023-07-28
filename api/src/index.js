5881457669e39b2809a5494cbe08913ed5f93ccc
Giuseppe-Pallazini <Pepepepito1883@gmail.com>
2023-07-28 08:25:41 -0300

    npm csv-parser FUNCIONANDO

--- api/src/index.js
+++ api/src/index.js
@@ -2,12 +2,24 @@ import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
 
+import fs from 'fs';
+import csv from 'csv-parser'
+
 const server = express();
 server.use(cors());
-server.use(express.json())
+server.use(express.json());
 
+const results = [];
 
-
-
+fs.createReadStream('movielist.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+    // [
+    //   { NAME: 'Daffy Duck', AGE: '24' },
+    //   { NAME: 'Bugs Bunny', AGE: '22' }
+    // ]
+  });
 
 server.listen(process.env.PORT, () => console.log(`API conectada na Porta ${process.env.PORT}`))
